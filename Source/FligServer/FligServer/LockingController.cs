65300a876c1b41af33ba3fe08093f1224514759d
Matthew.Rouse <matthew.rouse@advancedcomputersoftware.com>
2015-08-20 08:49:33 +0100

    Basic FligServerApi created

---
+++ Source/FligServer/FligServer/LockingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FligServer
+{
+    [RoutePrefix("lockingcontroller")]
+    public class LockingController : ApiController
+    {
+        [Route("lock/{file}")]
+        public IHttpActionResult Get(string file)
+        {
+            return Ok("Hello World");
+        }
+    }
+}
