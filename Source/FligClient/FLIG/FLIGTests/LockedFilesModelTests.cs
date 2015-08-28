0e1452655126031da5d7f81fe2a6eabdadb914c4
Matthew.Rouse <matthew.rouse@advancedcomputersoftware.com>
2015-08-28 13:14:16 +0100

    Invalid request test added

--- Source/FligClient/FLIG/FLIGTests/LockedFilesModelTests.cs
+++ Source/FligClient/FLIG/FLIGTests/LockedFilesModelTests.cs
@@ -19,11 +19,35 @@ namespace GivenARequest
         }
 
         [Fact]
-        public void ThenCheckTheLockFileResponseIsCompleted()
+        public void ThenCheckTheLockFileResponseIsTrue()
         {
             moqRestClient.Setup(x => x.Execute(It.IsAny<IRestRequest>()))
-                .Returns(new RestResponse() {ResponseStatus = ResponseStatus.Completed});
+                .Returns(new RestResponse() { ResponseStatus = ResponseStatus.Completed });
             Assert.True(lockedFileModel.LockFile(aFakeFile));
+        }
+
+
+    }
+
+    public class WhenTheRequestIsInvalid
+    {
+        private string aFakeFile;
+        private Mock<IRestClient> moqRestClient;
+        private LockedFilesModel lockedFileModel;
+
+        public WhenTheRequestIsInvalid()
+        {
+            aFakeFile = "testFile.txt";
+            moqRestClient = new Mock<IRestClient>();
+            lockedFileModel = new LockedFilesModel(moqRestClient.Object);
+        }
+
+        [Fact]
+        public void ThenCheckTheLockFileResponseIsFalse()
+        {
+             moqRestClient.Setup(x => x.Execute(It.IsAny<IRestRequest>()))
+                .Returns(new RestResponse() {ResponseStatus = ResponseStatus.Error});
+            Assert.False(lockedFileModel.LockFile(aFakeFile));           
         }
     }
 }
