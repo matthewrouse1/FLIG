674e9bcfec12e741cfa3572743cba58e79ddc208
Matthew.Rouse <matthew.rouse@advancedcomputersoftware.com>
2015-09-04 16:23:57 +0100

    Added git commit and git add

--- Source/FligClient/FLIG/FLIG/Git/GitModel.cs
+++ Source/FligClient/FLIG/FLIG/Git/GitModel.cs
@@ -12,83 +12,93 @@ namespace FligClient.Git
 {
     public class GitModel
     {
-        public class Git
+        public PullOptions pullOptions { get; private set; }
+        public PushOptions pushOptions { get; private set; }
+        public FetchOptions fetchOptions { get; private set; }
+        public CloneOptions cloneOptions { get; private set; }
+        public StatusOptions statusOptions { get; private set; }
+
+        public SecureUsernamePasswordCredentials credentials { get; private set; }
+        public LibGit2Sharp.Repository repository { get; private set; }
+        public Signature signature { get; private set; }
+
+        private void setupOptions(string Username, SecureString SecurePassword, string RepoPath, string RepoUrl)
         {
-            public PullOptions pullOptions { get; private set; }
-            public PushOptions pushOptions { get; private set; }
-            public FetchOptions fetchOptions { get; private set; }
-            public CloneOptions cloneOptions { get; private set; }
-            public StatusOptions statusOptions { get; private set; }
-
-            public SecureUsernamePasswordCredentials credentials { get; private set; }
-            public LibGit2Sharp.Repository repository { get; private set; }
-            public Signature signature { get; private set; }
-
-            private void setupOptions(string Username, SecureString SecurePassword, string RepoPath, string RepoUrl)
-            {
-                credentials = new SecureUsernamePasswordCredentials() {Username = Username, Password = SecurePassword};
-                var credentialsProvider =
-                    new CredentialsHandler((_url, _user, _cred) => new SecureUsernamePasswordCredentials
-                    {
-                        Username = Username,
-                        Password = SecurePassword
-                    });
-                fetchOptions = new FetchOptions() {CredentialsProvider = credentialsProvider};
-                pullOptions = new PullOptions() {FetchOptions = fetchOptions};
-                pushOptions = new PushOptions() {CredentialsProvider = credentialsProvider};
-                cloneOptions = new CloneOptions() {CredentialsProvider = credentialsProvider};
-                statusOptions = new StatusOptions() {ExcludeSubmodules = true, IncludeUnaltered = false};
-            }
-
-            private void setSignature(string Username, string EmailAddress)
-            {
-                this.signature = new Signature(Username, EmailAddress, DateTimeOffset.Now);
-            }
-
-            public string CloneRepo(string Url, string CloneToDirectory)
-            {
-                return LibGit2Sharp.Repository.Clone(Url, CloneToDirectory, this.cloneOptions);
-            }
-
-            public Git(string Username, string EmailAddress, SecureString Password,
-                string RepoPath = @"C:\repos\formsLibrary",
-                string RepoUrl = "http://github.com/AdvancedLegal/FormsLibrary")
-            {
-                // Password should be in the form of:
-                // var password = new System.Security.SecureString();
-                // "Welcome4".ToCharArray().ToList().ForEach(c => password.AppendChar(c));
-
-                this.setupOptions(Username, Password, RepoPath, RepoUrl);
-
-                this.setSignature(Username, EmailAddress);
-
-                if (!Directory.Exists(RepoPath))
+            credentials = new SecureUsernamePasswordCredentials() {Username = Username, Password = SecurePassword};
+            var credentialsProvider =
+                new CredentialsHandler((_url, _user, _cred) => new SecureUsernamePasswordCredentials
                 {
-                    this.CloneRepo(RepoUrl, RepoPath);
-                }
+                    Username = Username,
+                    Password = SecurePassword
+                });
+            fetchOptions = new FetchOptions() {CredentialsProvider = credentialsProvider};
+            pullOptions = new PullOptions() {FetchOptions = fetchOptions};
+            pushOptions = new PushOptions() {CredentialsProvider = credentialsProvider};
+            cloneOptions = new CloneOptions() {CredentialsProvider = credentialsProvider};
+            statusOptions = new StatusOptions() {ExcludeSubmodules = true, IncludeUnaltered = false};
+        }
 
-                if (!RepoPath.EndsWith("\\.git\\"))
-                    RepoPath += "\\.git\\";
+        private void setSignature(string Username, string EmailAddress)
+        {
+            this.signature = new Signature(Username, EmailAddress, DateTimeOffset.Now);
+        }
 
-                this.repository = new Repository(RepoPath);
+        public string CloneRepo(string Url, string CloneToDirectory)
+        {
+            return LibGit2Sharp.Repository.Clone(Url, CloneToDirectory, this.cloneOptions);
+        }
 
-            }
+        public GitModel(string Username, string EmailAddress, SecureString Password,
+            string RepoPath,
+            string RepoUrl = "http://github.com/AdvancedLegal/FormsLibrary")
+        {
+            // Password should be in the form of:
+            // var password = new System.Security.SecureString();
+            // "Welcome4".ToCharArray().ToList().ForEach(c => password.AppendChar(c));
 
-            public MergeResult Pull()
+            this.setupOptions(Username, Password, RepoPath, RepoUrl);
+
+            this.setSignature(Username, EmailAddress);
+
+            if (!Directory.Exists(RepoPath))
             {
-                return this.repository.Network.Pull(this.signature, this.pullOptions);
+                this.CloneRepo(RepoUrl, RepoPath);
             }
 
-            public void Push(Branch Branch = null)
-            {
-                Branch = Branch ?? this.repository.Branches["master"];
-                this.repository.Network.Push(Branch, this.pushOptions);
-            }
+            if (string.IsNullOrEmpty(RepoPath))
+                RepoPath = UserInfo.RepoDir;
 
-            public RepositoryStatus Status()
-            {
-                return this.repository.RetrieveStatus(statusOptions);
-            }
+            if (!RepoPath.EndsWith("\\.git\\"))
+                RepoPath += "\\.git\\";
+
+            this.repository = new Repository(RepoPath);
+
+        }
+
+        public MergeResult Pull()
+        {
+            return this.repository.Network.Pull(this.signature, this.pullOptions);
+        }
+
+        public void Push(Branch Branch = null)
+        {
+            Branch = Branch ?? this.repository.Branches["master"];
+            this.repository.Network.Push(Branch, this.pushOptions);
+        }
+
+        public void Add(List<string> files)
+        {
+            files.ForEach((file => this.repository.Stage(file)));
+        }
+
+        public void Commit(string message)
+        {
+            this.repository.Commit(message);
+        }
+
+        public RepositoryStatus Status()
+        {
+            return this.repository.RetrieveStatus(statusOptions);
         }
     }
 }
